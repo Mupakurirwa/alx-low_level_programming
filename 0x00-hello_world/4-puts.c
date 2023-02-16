a1a3a4f2a8b46c06785e0b9c8933f54b08070b9f
Mupakurirwa <mupakurirwal@gmail.com>
2023-02-16 12:19:39 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main - Program that prints a line with puts function
  * Return:0
- * */
-int main(void) 
+ */
+int main(void)
 {
-	puts("\"Programming is like a building a multillungial puzzle");
+	puts("\ "Programming is like a building a multillungial puzzle");
 	return(0);
 }

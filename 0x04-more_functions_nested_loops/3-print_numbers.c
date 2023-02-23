6e20ba108adb9f869cbe267d75e075195aff4103
Mupakurirwa <mupakurirwal@gmail.com>
2023-02-23 06:16:26 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/*
+ * 3-print_numbers.c
+ * The numbers speak for themselves
+ */
+
+#include "holberton.h"
+
+/**
+ * print_numbers - Prints the numbers from 0-9.
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+
+	_putchar('\n');
+}
